5fabcfb9bad461075962a82a7f069ead21323b00
Victor <victormatosbenazzi@gmail.com>
2023-12-01 22:09:23 -0300

    bg dobra 1

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,29 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			Animation: {
+				blob: "blob 1s infinite"
+			},
+			
+			keyframes: {
+				blob: {
+					"0%": {
+						transform: "scale(1)",
+					},
+					"33%": {
+						transform: "scale(1.5)",
+					},
+					"66%": {
+						transform: "scale(0.5)",
+					},
+					"100%": {
+						transform: "scale(1)",
+					}
+				}
+			}
+		},
 	},
+
 	plugins: [],
 }
