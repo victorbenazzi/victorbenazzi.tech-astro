51051323ecdd664490c042fd44f69539c6ebe49d
Victor <victormatosbenazzi@gmail.com>
2024-01-10 23:15:59 -0300

    Add react-fast-marquee package and update styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,26 +7,8 @@ export default {
 			'yellowBranding': '#EDCA4C',
 
 		  },
-			Animation: {
-				blob: "blob 1s infinite"
-			},
 			
-			keyframes: {
-				blob: {
-					"0%": {
-						transform: "scale(1)",
-					},
-					"33%": {
-						transform: "scale(1.5)",
-					},
-					"66%": {
-						transform: "scale(0.5)",
-					},
-					"100%": {
-						transform: "scale(1)",
-					}
-				}
-			}
+		
 		},
 	},
 
