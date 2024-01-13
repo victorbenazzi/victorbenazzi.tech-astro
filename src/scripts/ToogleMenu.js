fe516a59df3b1b5272367e21a293031b5f9a9aaa
Victor <victormatosbenazzi@gmail.com>
2024-01-13 17:09:10 -0300

    Update MainButton and Header components

---
+++ src/scripts/ToogleMenu.js
@@ -0,0 +1,5 @@
+    const BntMenuMobile = document.getElementById("BtnMenuMobile");
+	const MenuMobile = document.getElementById("MenuMobile");
+	BntMenuMobile.addEventListener("click", () => {
+	MenuMobile.classList.toggle("hidden");
+  });
