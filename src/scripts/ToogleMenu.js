c780d19465008178803f02c482912cb57c2b22a3
Victor <victormatosbenazzi@gmail.com>
2024-07-10 21:13:00 -0300

    refactor: update social media links in SocialIcons component

--- src/scripts/ToogleMenu.js
+++
@@ -1,5 +0,0 @@
-    const BntMenuMobile = document.getElementById("BtnMenuMobile");
-	const MenuMobile = document.getElementById("MenuMobile");
-	BntMenuMobile.addEventListener("click", () => {
-	MenuMobile.classList.toggle("hidden");
-  });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,19 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	
-	theme: {
-		extend: {colors: {
-			'indigo-hover': '#1F212C',
-			'yellowBranding': '#EDCA4C',
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 
-		  },
-		
-		},
-	},
+  theme: {
+    extend: {
+      colors: {
+        "indigo-hover": "#1F212C",
+        yellowBranding: "#EDCA4C",
+      },
+      backgroundImage: {
+        "bg-hero-desktop": "url('./src/assets/bg-desktop.png')",
+        "bg-hero-mobile": "url('./src/assets/bg-mobile.png')",
+      },
+    },
+  },
 
-	plugins: [
-		require('tailwind-scrollbar'),
-	],
-}
+  plugins: [require("tailwind-scrollbar")],
+};
